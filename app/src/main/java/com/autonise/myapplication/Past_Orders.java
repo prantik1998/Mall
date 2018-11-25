c6a5fd0b1bad9a6a510c1d67a4f10d35a0d56846
prantik972 <Prantik972@bitbucket.org>
2018-11-25 23:13:23 +0530

    prantik

---
+++ app/src/main/java/com/autonise/myapplication/Past_Orders.java
@@ -0,0 +1,13 @@
+package com.autonise.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Past_Orders extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past__orders);
+    }
+}
---
+++ app/src/main/java/com/autonise/myapplication/Privacy_Policy.java
@@ -0,0 +1,13 @@
+package com.autonise.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Privacy_Policy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy__policy);
+    }
+}
